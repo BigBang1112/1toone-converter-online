993f69670c7dd841bbf71341cfb7c87531a9bb90
Petr Pivoňka <petrpiv1@gmail.com>
2022-07-01 00:43:37 +0200

    Use ConversionService with Map

---
+++ 1toOneConverterOnline/Models/Map.cs
@@ -0,0 +1,28 @@
+﻿using GBX.NET;
+using GBX.NET.Engines.Game;
+
+namespace _1toOneConverterOnline.Models;
+
+public class Map
+{
+    public CGameCtnChallenge Challenge { get; init; }
+    
+    public Vec3 GridSize { get; set; }
+    public Vec3 GridOffset { get; set; }
+
+    public Map(CGameCtnChallenge challenge)
+    {
+        Challenge = challenge;
+    }
+
+    public void PlaceMinimalItem(Settings.Conversion.MinimalItem item)
+    {
+        if (item.Meta is null)
+        {
+            return;
+        }
+
+        var anchoredObject = Challenge.PlaceAnchoredObject(item.Meta, item.ItemCoords, item.Rot);
+        anchoredObject.BlockUnitCoord = (Byte3)item.BlockCoords;
+    }
+}
