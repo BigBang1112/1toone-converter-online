2f4e0f4583c3c6038e2a57d487f297e69200be91
Petr Pivoňka <petrpiv1@gmail.com>
2024-06-13 22:16:22 +0200

    Implement MultiBlockAddConversion

--- 1toOneConverterOnline/Models/Settings/Conversion/MultiBlockAddConversion.cs
+++ 1toOneConverterOnline/Models/Settings/Conversion/MultiBlockAddConversion.cs
@@ -1,14 +1,42 @@
-﻿namespace _1toOneConverterOnline.Models.Settings.Conversion;
+﻿using GBX.NET;
+using GBX.NET.Engines.Game;
+using System.Xml.Serialization;
+
+namespace _1toOneConverterOnline.Models.Settings.Conversion;
 
 public sealed class MultiBlockAddConversion : Conversion
 {
     public Block? NewBlock { get; init; }
+
+    [XmlElement]
     public ElementValue<byte> XStep { get; init; }
+    [XmlElement]
     public ElementValue<byte> YValue { get; init; }
+    [XmlElement]
     public ElementValue<byte> ZStep { get; init; }
 
     public override void Convert(Map map)
     {
-        throw new NotImplementedException();
+        map.Challenge.Chunks.Get<CGameCtnChallenge.Chunk0304301F>()!.Version = 6;
+
+        if (NewBlock is null || NewBlock.BlockName is null || map.Challenge.Blocks is null)
+        {
+            return;
+        }
+
+        for (var x = 1; x <= map.Challenge.Size.X; x += XStep.Value)
+        {
+            for (int z = 1; z <= map.Challenge.Size.Z; z += ZStep.Value)
+            {
+                map.Challenge.Blocks.Add(new CGameCtnBlock
+                {
+                    Name = NewBlock.BlockName,
+                    Direction = (Direction)NewBlock.Rot.Value,
+                    Coord = new(x, YValue.Value, z),
+                    Flags = NewBlock.Flags.Value,
+                    Author = NewBlock.Author?.Content
+                });
+            }
+        }
     }
 }
