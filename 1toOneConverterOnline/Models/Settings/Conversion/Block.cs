65d8e38f33515dad0deedf8e9ddacdf9847bfbfe
Petr Pivoňka <petrpiv1@gmail.com>
2024-06-13 22:15:03 +0200

    Implement Block

--- 1toOneConverterOnline/Models/Settings/Conversion/Block.cs
+++ 1toOneConverterOnline/Models/Settings/Conversion/Block.cs
@@ -1,5 +1,18 @@
-﻿namespace _1toOneConverterOnline.Models.Settings.Conversion;
+﻿using System.Xml.Serialization;
+
+namespace _1toOneConverterOnline.Models.Settings.Conversion;
 
 public sealed class Block
 {
+    public Id? BlockName { get; set; }
+
+    [XmlElement]
+    public ElementValue<byte> Rot { get; init; }
+
+    public Int3 Coords { get; set; }
+
+    [XmlElement]
+    public ElementValue<int> Flags { get; init; }
+
+    public Id? Author { get; set; }
 }
